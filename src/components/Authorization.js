98ca0a9e7d8db0f46ed834e8c9bab6c75f96e753
CORP\Arseny.Vaitekhovsky <avaitsekhoyski@servicechannel.com>
2021-05-30 17:07:46 +0300

    Changed API port

--- src/components/Authorization.js
+++ src/components/Authorization.js
@@ -26,7 +26,7 @@ export default function Authorization(props) {
     const [error, setError] = useState();
 
     const signIn = () => {
-        postData('https://localhost:5000/api/Account/auth', { Login: login, Password: password })
+        postData('https://localhost:8895/api/Account/auth', { Login: login, Password: password })
             .then((data) => {
                 console.log(data); // JSON data parsed by `response.json()` call
                 if (data.success) {
