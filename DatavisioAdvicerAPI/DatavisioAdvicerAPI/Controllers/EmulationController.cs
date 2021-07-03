2ededf6e8705cbf150b7ce8e3b4c69ce128b7e44
CORP\Arseny.Vaitekhovsky <avaitsekhoyski@servicechannel.com>
2021-07-04 00:41:18 +0300

    get history end-point

--- DatavisioAdvicerAPI/DatavisioAdvicerAPI/Controllers/EmulationController.cs
+++ DatavisioAdvicerAPI/DatavisioAdvicerAPI/Controllers/EmulationController.cs
@@ -13,7 +13,34 @@ namespace DatavisioAdvicerAPI.Controllers
     [ApiController]
     public class EmulationController : ControllerBase
     {
-        [HttpPost("addHistory")]
+        [HttpGet("history")]
+        public async Task<IActionResult> GetHistory(Guid userId)
+        {
+            List<EmulationHistoryEntity> history = new List<EmulationHistoryEntity>();
+            try
+            {
+                using (DatabaseContext db = new DatabaseContext())
+                {
+                    history = db.EmulationHistories.Where(x => x.UserId == userId).ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new
+                {
+                    success = false,
+                    errorMessage = e.Message
+                });
+            }
+
+            return Ok(new
+            {
+                success = true,
+                history = history,
+            });
+        }
+
+        [HttpPost("History")]
         public async Task<IActionResult> AddHistory(EmulationHistoryEntity model)
         {
             try
